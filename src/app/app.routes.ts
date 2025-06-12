55e107352beac16b265d0f3eab669551c6c6b06c
Tamilstemz <fsdev3@stemzglobal.com>
2025-06-12 19:43:25 +0530

    Tamil Book to Calendar Chnages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { FrontpageComponent } from './frontpage/frontpage.component';
 import { BookAppointmentComponent } from './book-appointment/book-appointment.component';
 import { TermsofuseComponent } from './termsofuse/termsofuse.component';
 import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
+import { ScheduleCalendarComponent } from './schedule-calendar/schedule-calendar.component';
 
 export const routes: Routes = [
   { path: '', component: FrontpageComponent },
-  { path: 'BookAppointment', component: BookAppointmentComponent },
+  { path: 'BookAppointment', component: ScheduleCalendarComponent },
   { path: 'TermsOfUse', component: TermsofuseComponent },
   { path: 'PrivacyPolicy', component: PrivacypolicyComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
