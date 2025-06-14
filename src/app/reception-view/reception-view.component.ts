44f4187d53aafbdc9bd6a9feb8a2a15b41587845
fsdev4 <fsdev4@stemzglobal.com>
2025-06-14 06:49:23 +0530

    change

--- src/app/reception-view/reception-view.component.ts
+++ src/app/reception-view/reception-view.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reception-view',
@@ -8,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './reception-view.component.css'
 })
 export class ReceptionViewComponent {
+  constructor(public router: Router) {}
+
+  navigateAndScroll(sectionId: string, event: Event): void {
+  event.preventDefault();
+    
+  const scrollToSection = () => {
+    const el = document.getElementById(sectionId);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      el.classList.add('highlighted');
+      setTimeout(() => el.classList.remove('highlighted'), 1000);
+    } else {
+      console.warn('Element not found for id:', sectionId);
+    }
+  };
+
+  // Navigate if the section is on a different route
+  if (sectionId === 'Book-Appointment') {
+    sectionId = 'singlenavbar';
+    this.router.navigate(['/BookAppointment']).then(() => {
+      setTimeout(scrollToSection, 100);
+    });
+  }
+}
 
 }
